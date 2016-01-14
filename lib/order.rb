44204af5fa323db03a46da00605fdc2c8f682c6e
thisdotrob <this.rob@icloud.com>
2016-01-14 19:06:44 +0000

    Add #initialize and #add_item to Order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,19 @@
 class Order
   attr_reader :customer_name
+  attr_reader :items
+
+  def initialize
+    @customer_name = ''
+    @items = {}
+  end
 
   def set_customer_name(name)
     @customer_name = name
+  end
+
+  def add_item(item)
+    item = item.to_sym
+    @items[item] ? @items[item] += 1 : @items[item] = 1
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,10 +3,27 @@ require 'order'
 describe Order do
   subject { Order.new }
 
+  context '#initialize' do
+    it 'is initialized with an empty list of items' do
+      expect(subject.items).to eq({})
+    end
+
+    it 'is initialized with an empty name' do
+      expect(subject.customer_name).to eq('')
+    end
+  end
+
   context '#set_customer_name' do
     it 'sets the customer name' do
       subject.set_customer_name('Bob')
       expect(subject.customer_name).to eq('Bob')
+    end
+  end
+
+  context '#add_item' do
+    it 'adds an item to the order' do
+      3.times { subject.add_item('Tiramisu') }
+      expect(subject.items).to eq({Tiramisu: 3})
     end
   end
 
