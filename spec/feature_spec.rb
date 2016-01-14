12dfff2075a7d9f5b6b5eace0a12c1660633197b
thisdotrob <this.rob@icloud.com>
2016-01-14 18:53:54 +0000

    Add placing order feature test

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -6,9 +6,23 @@ describe 'Feature' do
   subject { Till.new(Order, 'spec/testmenu.json') }
 
   context 'till startup' do
-    it 'starts with an empty order and a menu' do
+    it 'starts with an empty order and a menu read from the .json' do
       expect(subject.order.is_a?(Order)).to be true
       expect(subject.menu).to eq(mock_menu)
+    end
+  end
+
+  context 'placing an order' do
+    it 'returns a receipt in the correct json string format' do
+      expected_items = [
+        { name: 'Cafe Latte', quantity: 5, total: 23.75 },
+        { name: 'Tiramisu', quantity: 1, total: 11.4 }
+      ]
+      expected_receipt = JSON.generate({ name: 'Bob', items: expected_items })
+      subject.add_name_to_order('Bob')
+      subject.add_item_to_order('Tiramisu')
+      5.times { subject.add_item_to_order('Cafe Latte') }
+      expect(subject.receipt).to eq(expected_receipt)
     end
   end
 
